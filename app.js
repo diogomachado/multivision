83941f1b708fc68385213d74de1fba12656d9814
Diogo Machado <diogo@diogomachado.com>
2022-10-04 13:03:05 -0300

    Code refactore, more clean

--- app.js
+++ app.js
@@ -1,51 +1,44 @@
 import Letter from './src/Letter.js'
 
+function createElement(type, className, content) {
+  const element = document.createElement(type)
+  if (className) {
+    element.classList.add(className)
+  }
+  if (content) {
+    element.textContent = content
+  }
+  return element
+}
+
 function createUserBlock(user) {
-  // Parent
   let app = document.querySelector('#app')
 
-  // Child
-  let userDiv = document.createElement('div')
-  userDiv.classList.add('user-wrapper')
+  let userDiv = createElement('div', 'user-wrapper')
+  let userHeader = createElement('header')
+  let userHeaderTitle = createElement('h4', 'title', user.name)
+  let userHeaderCompany = createElement('p', 'company', user.company)
 
-  // Header
-  let userHeader = document.createElement('header')
-  let userHeaderTitle = document.createElement('h4')
-  userHeaderTitle.classList.add('title')
-  userHeaderTitle.textContent = user.name
-  let userHeaderCompany = document.createElement('p')
-  userHeaderCompany.classList.add('company')
-  userHeaderCompany.textContent = user.company
   userHeader.appendChild(userHeaderTitle)
   userHeader.appendChild(userHeaderCompany)
   userDiv.appendChild(userHeader)
 
   // Child 'posts'
-  let postsDiv = document.createElement('div')
-  postsDiv.classList.add('user-posts')
-
-  let postTitle = document.createElement('h5')
-  postTitle.classList.add('title')
-  postTitle.textContent = 'Posts'
-
-  let postContainerDiv = document.createElement('div')
-  postContainerDiv.classList.add('user-posts-container')
+  let postsDiv = createElement('div', 'user-posts')
+  let postTitle = createElement('h5', 'title', 'Posts')
+  let postContainerDiv = createElement('div', 'user-posts-container')
 
   postsDiv.appendChild(postTitle)
 
-  let posts = user.posts
   // Looping posts
-  posts.forEach((post) => {
-    let postDiv = document.createElement('div')
-    postDiv.classList.add('post')
-
-    let postTitle = document.createElement('p')
-    postTitle.textContent = post.title
+  user.posts.forEach((post) => {
+    let postDiv = createElement('div', 'post')
+    let postTitle = createElement('p', undefined, post.title)
 
     postDiv.appendChild(postTitle)
     postContainerDiv.appendChild(postDiv)
   })
-  console.log(postContainerDiv)
+
   postsDiv.appendChild(postContainerDiv)
   userDiv.appendChild(postsDiv)
 
@@ -57,25 +50,6 @@ function createUserBlock(user) {
 const letter = new Letter()
 const responseJson = await letter.get()
 
-// Create the blocks
-{
-  /* <div class="user-wrapper">
-  <header>
-    <h4 class="title">Diogo Machado</h4>
-    <p class="company">DM Technology</p>
-  </header>
-  <div class="user-posts">
-    <h5 class="title">Posts</h5>
-    <div class="user-posts-container">
-      <div class="post">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, assumenda!</p>
-      </div>
-    </div>
-  </div>
-</div> */
-}
-const users = responseJson
-
-users.forEach((user) => {
+responseJson.forEach((user) => {
   createUserBlock(user)
 })
